4fbf94d4de94e47a4e569bca177087de06138e7d
Peter Kiraly <peter.kiraly@gwdg.de>
2022-03-15 17:59:49 +0100

    Count collocations

---
+++ collocations-count.php
@@ -0,0 +1,72 @@
+<?php
+
+$catalogues = [
+  'bnpl', 'bayern', 'ddb', 'dnb', 'kbr', 'libris', 'lnb', 'loc', 'mek', 'nfi', 'onb', 'uva', 'K10plus'
+];
+
+$fields = [];
+$fields[] = $argv[1];
+$fields[] = $argv[2];
+$id = $argv[3];
+
+// $fields = ['leader07_bibliographicLevel_ss', 'leader06_typeOfRecord_ss'];
+// leader05_recordStatus_ss
+// leader06_typeOfRecord_ss
+// leader07_bibliographicLevel_ss
+// 040d_AdminMetadata_modifyingAgency_ss
+
+collocateCatalogues($catalogues, $fields);
+
+/// functions
+
+function collocateCatalogues($catalogues, $fields) {
+  echo str_putcsv(['catalogue', $fields[0], $fields[1], 'count']), "\n";
+  foreach ($catalogues as $catalogue) {
+  	collocateFields($catalogue, $fields);
+  }
+}
+
+function collocateFields($catalogue, $fields) {
+  $q = [];
+  foreach ($fields as $field) {
+    $$q[] = sprintf('%s:*', $field);
+  }
+
+  $params = 'q=' . join(' AND ', $q) . '&start=0&rows=0&wt=json&q.op=AND&json.nl=map';
+
+  $response = search($catalogue, $params);
+  print_r($response);
+
+  /*
+  $f1_values = $response->facets->{$fields[0]};
+  foreach ($f1_values as $value1 => $count1) {
+    $response = search($catalogue, $params . '&fq=' . $fields[0] . ':' . urlencode('"' . $value1 . '"'));
+    foreach ($response->facets->{$fields[1]} as $value2 => $count2) {
+      echo str_putcsv([$catalogue, $value1, $value2, $count2]), "\n";
+    }
+  }
+  */
+}
+
+function search($catalogue, $params) {
+  $url = 'http://localhost:8983/solr/' . $catalogue . '/select?' . $params;
+  $solrResponse = json_decode(file_get_contents($url));
+  $response = (object)[
+    'numFound' => $solrResponse->response->numFound,
+    'docs' => $solrResponse->response->docs,
+    'facets' => (isset($solrResponse->facet_counts) ? $solrResponse->facet_counts->facet_fields : []),
+    'params' => $solrResponse->responseHeader->params,
+  ];
+
+  return $response;
+}
+
+function str_putcsv(array $input, $delimiter = ',', $enclosure = '"') {
+  $fp = fopen('php://temp', 'r+b');
+
+  fputcsv($fp, $input, $delimiter, $enclosure);
+  rewind($fp);
+  $data = rtrim(stream_get_contents($fp), "\n");
+  fclose($fp);
+  return $data;
+}
---
+++ ungvary-counts.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+rm ungvary-counts.csv
+
+echo "1/12) LEADER 06 versus LEADER 07"
+php collocations-count.php leader06_typeOfRecord_ss leader07_bibliographicLevel_ss ldr06-ldr07 >> ungvary-counts.csv
+echo "2/12) LEADER 06 versus LEADER 17"
+php collocations-count.php leader06_typeOfRecord_ss leader17_encodingLevel_ss ldr06-ldr17 >> ungvary-counts.csv
+echo "3/12) LEADER 07 versus LEADER 17"
+php collocations-count.php leader07_bibliographicLevel_ss leader17_encodingLevel_ss ldr07-ldr17 >> ungvary-counts.csv
+echo "4/12) LEADER 06 versus 007/00"
+php collocations-count.php leader06_typeOfRecord_ss 007common00_PhysicalDescription_categoryOfMaterial_ss ldr06-00700 >> ungvary-counts.csv
+echo "5/12) LEADER 07 versus 007/00"
+php collocations-count.php leader07_bibliographicLevel_ss 007common00_PhysicalDescription_categoryOfMaterial_ss ldr07-00700 >> ungvary-counts.csv
+echo "6/12) LEADER 17 versus 007/00"
+php collocations-count.php leader17_encodingLevel_ss 007common00_PhysicalDescription_categoryOfMaterial_ss ldr17-00700 >> ungvary-counts.csv
+echo "7/12) LEADER 06 versus 007 00 a 01"
+php collocations-count.php leader06_typeOfRecord_ss 007map01_PhysicalDescription_specificMaterialDesignation_ss ldr06-007map01material >> ungvary-counts.csv
+echo "8/12) LEADER 06 versus 007 00 t 01"
+php collocations-count.php leader06_typeOfRecord_ss 007text01_PhysicalDescription_specificMaterialDesignation_ss ldr06-007text01material >> ungvary-counts.csv
+echo "9/12) 007 a 01 versus 008 18–21"
+php collocations-count.php 007map01_PhysicalDescription_specificMaterialDesignation_ss 008book18_GeneralInformation_illustrations_ss 007map01material-008book18ill >> ungvary-counts.csv
+echo "10/12) 007 00 t 01 versus 008 18-21"
+php collocations-count.php 007text01_PhysicalDescription_specificMaterialDesignation_ss 008book18_GeneralInformation_illustrations_ss 007text01material-008book18ill >> ungvary-counts.csv
+echo "11/12) 007 00 t 01 versus 008 24-27"
+php collocations-count.php 007text01_PhysicalDescription_specificMaterialDesignation_ss 008book24_GeneralInformation_natureOfContents_ss 007text01material-008book24nature >> ungvary-counts.csv
+echo "12/12) 007 00 t 01 versus 008 33"
+php collocations-count.php 007text01_PhysicalDescription_specificMaterialDesignation_ss 008book33_GeneralInformation_literaryForm_ss 007text01material-008book33lit >> ungvary-counts.csv
+
+echo DONE
